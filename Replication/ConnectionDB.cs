73ec9f5650e578818da85cbb2f3a7603f849e107
huynh1999 <55187798+huynh1999@users.noreply.github.com>
2020-10-20 23:07:44 +0700

    master

---
+++ Replication/ConnectionDB.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Replication
+{
+    class ConnectionDB
+    {
+        public SqlConnection GetConnection()
+        {
+            string s = "Data Source=192.168.22.134;Initial Catalog=test;User ID=huynh;Password=123456";
+            SqlConnection conn = new SqlConnection(s);
+            conn.Open();
+            if (conn.State == ConnectionState.Open)
+            {
+                Console.WriteLine("Connection ok");
+            }
+            else
+            {
+                Console.WriteLine("Connection error");
+            }
+            return conn;
+        }
+    }
+}
---
+++ Replication/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+namespace Replication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ConnectionDB db = new ConnectionDB();
+            SqlConnection conn = db.GetConnection();
+            DataTable table = new DataTable();
+            if (conn.State == ConnectionState.Open)
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter("select * from Persons", conn);
+                adapter.Fill(table);
+                for(int i = 0; i < table.Rows.Count; i++)
+                {
+                    
+                    textBox2.Text += table.Rows[i][1].ToString();
+                }
+                textBox2.Text += "Connection ok";
+            }
+            else
+            {
+                textBox2.Text = "Connection error";
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
